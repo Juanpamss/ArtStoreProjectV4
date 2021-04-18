811ef5aea21832f8b2d2b19b6dff9e1d58ca3ef6
andreahowes <andrea.howes01@gmail.com>
2021-04-18 16:58:26 -0400

    added 5 6 7

--- src/app/details-modal/details-modal.component.ts
+++ src/app/details-modal/details-modal.component.ts
@@ -1,5 +1,7 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import {ArtPiece} from "../models/artPiece.model";
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-details-modal',
@@ -9,10 +11,18 @@ import {ArtPiece} from "../models/artPiece.model";
 export class DetailsModalComponent implements OnInit {
 
   @Input('ArtPiece') artPiece: ArtPiece;
+  @Output() cartCount: EventEmitter<any> = new EventEmitter();
+
+  inCart: boolean;
+  itemCount: Number;
 
   thumbImage
 
-  constructor() { }
+  constructor(
+    private cartService: CartService,
+    private modalService: NgbModal,
+
+  ) { }
 
   ngOnInit(): void {
 
@@ -20,6 +30,23 @@ export class DetailsModalComponent implements OnInit {
 
     console.log(this.thumbImage)
 
+  }
+
+  addToCart(content) {
+    this.inCart = true;
+    this.cartService.addToCart(this.artPiece);
+    this.itemCount= this.cartService.getCount();
+    this.cartCount.emit(this.itemCount);
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then((result) => {
+      })
+  }
+
+  closeCart() {
+    this.modalService.dismissAll();
+  }
+  closeAddedToCartModal() {
+    this.modalService.dismissAll();
   }
 
 }
